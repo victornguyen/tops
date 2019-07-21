f407e8b2ec6257c4706055ae8392ba9bac217729
Victor Nguyen <vic.nguyen@gmail.com>
2019-07-21 16:57:30 +1000

    Filter products by size

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useProducts from './hooks/useProducts'
+import SizeFilter from './SizeFilter'
 
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
   const [products, isLoading] = useProducts()
+  const [selectedSize, setSize] = useState('')
+
+  // Size filter change handler
+  const handleSizeFilter = event => {
+    setSize(event.target.value)
+  }
+
+  // Filter products by size if one is selected
+  const filteredProducts = selectedSize
+    ? products.filter(({ size }) => size.includes(selectedSize))
+    : products
 
   return isLoading ? (
     <div>
@@ -13,13 +25,19 @@ function App() {
       LOADERING...
     </div>
   ) : (
-    <section>
-      {products.map(({ index, productName, price }) => (
-        <div key={index}>
-          {productName} {price}
-        </div>
-      ))}
-    </section>
+    <div>
+      <header>
+        Women&rsquo;s tops
+        <SizeFilter size={selectedSize} onChange={handleSizeFilter} />
+      </header>
+      <main>
+        {filteredProducts.map(({ index, productName, price }) => (
+          <div key={index}>
+            {productName} {price}
+          </div>
+        ))}
+      </main>
+    </div>
   )
 }
 
---
+++ src/SizeFilter.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SizeFilter = ({ size, onChange }) => (
+  <select value={size} onChange={onChange}>
+    <option value="">Filter by size</option>
+    <option value="XS">XS</option>
+    <option value="S">S</option>
+    <option value="M">M</option>
+    <option value="L">L</option>
+    <option value="XL">XL</option>
+  </select>
+)
+
+SizeFilter.propTypes = {
+  size: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+}
+
+export default SizeFilter
