4520f10462393467d974056a1b38ba9e47c6a09c
Victor Nguyen <vic.nguyen@gmail.com>
2019-07-21 21:01:33 +1000

    Seperate product listing into own component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import useProducts from '../hooks/useProducts'
 import sizes from '../styles/sizes'
+import useProducts from '../hooks/useProducts'
 
 import Header from './Header'
-import Product from './Product'
+import Listing from './Listing'
 
 const Container = styled.div`
   width: 100%;
@@ -12,27 +12,6 @@ const Container = styled.div`
   @media (min-width: ${sizes.viewport4}) {
     width: 90%;
     margin: 20px auto;
-  }
-`
-
-const ProductList = styled.main`
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 10px;
-  border-top: 1px solid #e1e1e1;
-  border-left: 1px solid #e1e1e1;
-`
-
-const ProductItem = styled(Product)`
-  width: 100%;
-  @media (min-width: ${sizes.viewport4}) {
-    width: 50%;
-  }
-  @media (min-width: ${sizes.viewport9}) {
-    width: calc(100% / 3);
-  }
-  @media (min-width: ${sizes.viewport12}) {
-    width: 25%;
   }
 `
 
@@ -50,27 +29,14 @@ function App() {
     ? products.filter(({ size }) => size.includes(selectedSize))
     : products
 
-  return isLoading ? (
-    <div>LOADERING...</div>
-  ) : (
+  return (
     <Container>
       <Header
         title="Women&rsquo;s tops"
         size={selectedSize}
         onSizeChange={handleSizeFilter}
       />
-      <ProductList>
-        {filteredProducts.map(product => (
-          <ProductItem
-            key={product.index}
-            name={product.productName}
-            price={product.price}
-            image={product.productImage}
-            isSale={product.isSale}
-            isExclusive={product.isExclusive}
-          />
-        ))}
-      </ProductList>
+      {isLoading ? 'LOADERING' : <Listing products={filteredProducts} />}
     </Container>
   )
 }
---
+++ src/components/Listing.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import sizes from '../styles/sizes'
+
+import Product from './Product'
+
+const List = styled.main`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 10px;
+  border-top: 1px solid #e1e1e1;
+  border-left: 1px solid #e1e1e1;
+`
+
+const Item = styled(Product)`
+  width: 100%;
+  @media (min-width: ${sizes.viewport4}) {
+    width: 50%;
+  }
+  @media (min-width: ${sizes.viewport9}) {
+    width: calc(100% / 3);
+  }
+  @media (min-width: ${sizes.viewport12}) {
+    width: 25%;
+  }
+`
+
+const Listing = ({ products }) => (
+  <List>
+    {products.map(product => (
+      <Item
+        key={product.index}
+        name={product.productName}
+        price={product.price}
+        image={product.productImage}
+        isSale={product.isSale}
+        isExclusive={product.isExclusive}
+      />
+    ))}
+  </List>
+)
+
+Listing.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      index: PropTypes.number,
+      isExclusive: PropTypes.bool,
+      isSale: PropTypes.bool,
+      price: PropTypes.string,
+      productImage: PropTypes.string,
+      productName: PropTypes.string,
+    })
+  ),
+}
+
+export default Listing
